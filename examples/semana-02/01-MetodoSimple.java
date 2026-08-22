a6d50f119a5dab19aac9b9d4a1a18eade8852c6b
Carlos F. Martínez Sánchez <carlos.f.martinez.s+github@gmail.com>
2026-08-22 11:33:13 -0400

    docs: agregar ejemplo aislado de metodo simple

---
+++ examples/semana-02/01-MetodoSimple.java
@@ -0,0 +1,12 @@
+public class MetodoSimple {
+
+    public static void main(String[] args) {
+        System.out.println("Antes de llamar al método");
+        mostrarSaludo();
+        System.out.println("Después de llamar al método");
+    }
+
+    public static void mostrarSaludo() {
+        System.out.println("Hola desde un método");
+    }
+}
