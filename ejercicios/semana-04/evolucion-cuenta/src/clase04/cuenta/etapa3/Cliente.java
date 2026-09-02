81689efedca90927acf90882807a42139134c264
Carlos F. Martínez Sánchez <carlos.f.martinez.s+github@gmail.com>
2026-09-02 12:56:12 -0400

    code: add Cliente for Cuenta stage 3

---
+++ ejercicios/semana-04/evolucion-cuenta/src/clase04/cuenta/etapa3/Cliente.java
@@ -0,0 +1,19 @@
+package clase04.cuenta.etapa3;
+
+public class Cliente {
+    private String nombre;
+    private String rut;
+
+    public Cliente(String nombre, String rut) {
+        this.nombre = nombre;
+        this.rut = rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+}
